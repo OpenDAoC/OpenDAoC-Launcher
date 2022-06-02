111be10ceb0a58a930254f3c245d6b125bb90ebf
clait <stefanomus@fastmail.com>
2022-06-02 11:04:46 +0100

    Updater background worker

--- WPFLauncher/MainWindow.xaml.cs
+++ WPFLauncher/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Windows;
 
 namespace WPFLauncher
 {
@@ -8,17 +10,47 @@ namespace WPFLauncher
     public partial class MainWindow : Window
     {
         private bool _updateAvailable;
+        
+        private BackgroundWorker _updateChecker;
         public MainWindow()
         {
             InitializeComponent();
-            _updateAvailable = Updater.CheckForNewVersion();
-            PlayButton.Content = _updateAvailable ? "Update" : "Play";
+            InitializeWorkers();
+
+        }
+        private void InitializeWorkers()
+        {
+            _updateChecker = new BackgroundWorker();
+            _updateChecker.DoWork += UpdateChecker_DoWork;
+            _updateChecker.RunWorkerCompleted += UpdateChecker_RunWorkerCompleted;
+            _updateChecker.WorkerReportsProgress = true;
         }
 
         private void playButton_Click(object sender, RoutedEventArgs e)
         {
-            if (_updateAvailable)
+            _updateChecker.RunWorkerAsync(2000);
+        }
+        
+        private void UpdateChecker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            _updateAvailable = Updater.CheckForNewVersion();
+            Dispatcher.Invoke(() =>
+            {
+                PlayButton.Content = _updateAvailable ? "Updating.." : "Play";
+            });
+
+            if (!_updateAvailable) return;
+            Thread.Sleep((int) e.Argument);
+            Dispatcher.Invoke(() =>
+            {
                 Updater.DownloadUpdates();
+            });
+        }
+        
+        
+        private void UpdateChecker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs runWorkerCompletedEventArgs)
+        { 
+            PlayButton.Content = "Play";
         }
     }
 }
